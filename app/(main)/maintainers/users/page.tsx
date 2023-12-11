500141be9dc433905f5864f67321112be4645cba
DESKTOP-SB1C0KM\basti <bastian.carrasco@triplea.cl>
2023-12-11 12:12:35 -0300

    user page

---
+++ app/(main)/maintainers/users/page.tsx
@@ -0,0 +1,241 @@
+"use client";
+import { ProductService } from "@/demo/service/ProductService";
+import { Button } from "primereact/button";
+import { Column } from "primereact/column";
+import { DataTable } from "primereact/datatable";
+import { InputText } from "primereact/inputtext";
+import { OverlayPanel } from "primereact/overlaypanel";
+import { Password } from "primereact/password";
+import { useEffect, useRef, useState } from "react";
+import { FaUserEdit } from "react-icons/fa";
+
+const Users = () => {
+  const [products, setProducts] = useState([]);
+  const [showForm, setShowForm] = useState(false);
+  const op: any = useRef<OverlayPanel>(null);
+  const [selectedProduct, setSelectedProduct] = useState(null);
+  const [formData, setFormData] = useState({
+    nombreUsuario: "",
+    rut: "",
+    telefono: "",
+    direccion: "",
+    email: "",
+    contraseña: "",
+    imagen: "",
+    gerencia: "",
+    centroCosto: "",
+  });
+
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+    if (
+      formData.nombreUsuario.trim() === "" ||
+      formData.rut.trim() === "" ||
+      formData.telefono.trim() === "" ||
+      formData.direccion.trim() === "" ||
+      formData.email.trim() === "" ||
+      formData.contraseña.trim() === "" ||
+      formData.imagen.trim() === "" ||
+      formData.gerencia.trim() === "" ||
+      formData.centroCosto.trim() === ""
+    ) {
+      // Mostrar mensaje de error o realizar alguna acción
+    } else {
+      // Enviar el formulario o realizar otras acciones
+    }
+  };
+
+  const handleChange = (event: any) => {
+    const { name, value } = event.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  useEffect(() => {
+    ProductService.getProductsMixed().then((data: any) => setProducts(data));
+  }, []);
+
+  const productSelect = (e: any) => {
+    setShowForm(true);
+    op.current.hide();
+    console.log(e);
+  };
+
+  return (
+    <div>
+      <p className="general-title">Mantenedor de Usuario</p>
+      <div className="w-full flex align-content-center mb-4">
+        <p className="general-text">Ingresar nombre de usuario</p>
+        <span className="p-input-icon-left ml-8">
+          <i className="pi pi-search" />
+          <InputText
+            style={{ width: "454px" }}
+            className="border-round-3xl"
+            placeholder="Buscar"
+            onChange={(e) => {
+              if (e.target.value.length > 3) {
+                op.current.toggle(e);
+              }
+            }}
+          />
+        </span>
+      </div>
+      {showForm && (
+        <div>
+          <div className="card">
+            <div className="flex">
+              <div className="w-full flex flex-column">
+                <div className="w-9 flex align-items-center p-3 mb-5">
+                  <FaUserEdit className="text-2xl text-primary mr-2" />
+                  <p className="text-2xl font-bold">AGREGAR O EDITAR USUARIO</p>
+                </div>
+                <form className="flex flex-wrap align-items-center">
+                  <div className="flex flex-column mb-8">
+                    <label htmlFor="nombreUsuario">Nombre de usuario</label>
+                    <InputText
+                      name="nombreUsuario"
+                      style={{ width: "500px" }}
+                      value={formData.nombreUsuario}
+                      onChange={handleChange}
+                      placeholder="Nombre de usuario"
+                    />
+                  </div>
+                  <div className="flex flex-column ml-4 mb-8">
+                    <label htmlFor="rut">RUT</label>
+                    <InputText
+                      name="rut"
+                      style={{ width: "300px" }}
+                      value={formData.rut}
+                      onChange={handleChange}
+                      placeholder="RUT"
+                    />
+                  </div>
+                  <div className="flex flex-column ml-4 mb-8">
+                    <label htmlFor="telefono">Teléfono</label>
+                    <InputText
+                      name="telefono"
+                      style={{ width: "300px" }}
+                      value={formData.telefono}
+                      onChange={handleChange}
+                      placeholder="Teléfono"
+                    />
+                  </div>
+                  <div className="flex flex-column mb-8">
+                    <label htmlFor="direccion">Dirección</label>
+                    <InputText
+                      name="direccion"
+                      style={{ width: "500px" }}
+                      value={formData.direccion}
+                      onChange={handleChange}
+                      placeholder="Dirección"
+                    />
+                  </div>
+                  <div className="flex flex-column ml-4 mb-8">
+                    <label htmlFor="email">Email</label>
+                    <InputText
+                      name="email"
+                      style={{ width: "300px" }}
+                      value={formData.email}
+                      onChange={handleChange}
+                      placeholder="Email"
+                    />
+                  </div>
+                  <div className="flex flex-column ml-4 mb-8">
+                    <label htmlFor="email">Contraseña</label>
+                    <Password
+                      name="contraseña"
+                      value={formData.contraseña}
+                      onChange={handleChange}
+                      placeholder="Contraseña"
+                      toggleMask
+                    />
+                  </div>
+                  <div className="flex flex-column mb-8">
+                    <label htmlFor="fileUpload">Imagen</label>
+                    <span className="p-input-icon-right">
+                      <i className="pi pi-upload" />
+                      <InputText
+                        name="fileUpload"
+                        style={{ width: "300px" }}
+                        value={formData.imagen}
+                        onChange={handleChange}
+                        placeholder="Sube/Actualiza una imagen"
+                      />
+                    </span>
+                  </div>
+                  <div className="flex flex-column ml-4 mb-8">
+                    <label htmlFor="gerencia">Gerencia</label>
+                    <InputText
+                      name="gerencia"
+                      style={{ width: "300px" }}
+                      value={formData.gerencia}
+                      onChange={handleChange}
+                      placeholder="Gerencia"
+                    />
+                  </div>
+                  <div className="flex flex-column ml-4 mb-8">
+                    <label htmlFor="centroCosto">Centro de costo</label>
+                    <InputText
+                      name="centroCosto"
+                      style={{ width: "300px" }}
+                      value={formData.centroCosto}
+                      onChange={handleChange}
+                      placeholder="Centro de costo"
+                    />
+                  </div>
+                </form>
+              </div>
+              <div className="w-3 flex align-items-center">
+                <div
+                  className="border-round bg-primary font-bold p-3 m-3 flex align-items-center justify-content-center"
+                  style={{ width: "200px", height: "200px" }}
+                >
+                  imagen
+                </div>
+              </div>
+            </div>
+            <div className="flex justify-content-end">
+              <Button label="Cancelar" />
+              <Button className="ml-3" label="Guardar" />
+            </div>
+          </div>
+        </div>
+      )}
+      <OverlayPanel ref={op}>
+        <DataTable
+          value={products}
+          selectionMode="single"
+          paginator
+          rows={5}
+          selection={selectedProduct}
+          onSelectionChange={(e: any) => setSelectedProduct(e.value)}
+          onRowClick={productSelect}
+        >
+          <Column
+            field="name"
+            header="Name"
+            sortable
+            style={{ minWidth: "12rem" }}
+          />
+          <Column
+            field="price"
+            header="Price"
+            sortable
+            style={{ minWidth: "8rem" }}
+          />
+        </DataTable>
+      </OverlayPanel>
+      {!showForm && (
+        <div>
+          <div className="p-4 absolute bottom-0 right-0 font-semibold text-xl flex align-items-center">
+            <div className="logo-sodimac"></div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Users;
